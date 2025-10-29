374b5a5aa847dbe170bf5f62cf17054cbb0890b4
midnight243 <midnight243@midnight243-pc>
2025-10-29 20:01:14 -0300

    feat: adicionar middleware para redirecionar / → /login

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone();
-
-  if (url.pathname === "/") {
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
+  if (req.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/"], 
 };
