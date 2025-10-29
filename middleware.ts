8209ffead532ad577533684c1b514e1b9d253e66
midnight243 <midnight243@midnight243-pc>
2025-10-29 19:59:29 -0300

    feat: adicionar middleware para redirecionar / → /login

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+
+  if (url.pathname === "/") {
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/"],
+};
